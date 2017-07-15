6594a408f101ed8a012b5d6ecac950b951fb133c
Sunshine Inca <sunshineinca@outlook.com>
2017-07-15 21:55:34 +0000

    what did you change

---
+++ sketch/main.js
@@ -0,0 +1,34 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBaV1MZiYi3qj5UhFRYC4y7OUjeJsiFmMU",
+    authDomain: "collaborative-sketch-2deb0.firebaseapp.com",
+    databaseURL: "https://collaborative-sketch-2deb0.firebaseio.com",
+    projectId: "collaborative-sketch-2deb0",
+    storageBucket: "",
+    messagingSenderId: "387149412315"
+};
+firebase.initializeApp(config);
+var pointsData = firebase.database().ref();
+
+function setup() {
+    createCanvas(400, 400);
+    background(255);
+    fill(0);
+    pointsData.on("child_added", function (point) {
+    points.push(point.val());
+  });
+}
+
+function draw() {
+    background(255);
+    
+    for (var i = 0; i < points.length; i++) {
+      var point = points[i];
+      ellipse(point.x, point.y, 5,5);
+      
+  }
+}    
+    }
+
+}
+
